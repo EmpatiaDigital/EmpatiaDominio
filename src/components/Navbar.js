8eff01885e35b9ce49f01798f129b3361879a7e6
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-04 11:29:51 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,7 +64,7 @@ export default function Navbar() {
           <Link to="/" onClick={handleLinkClick}>Inicio</Link>
         </li>
         <li>
-          <Link to="/actividades" onClick={handleLinkClick}>Curso I.A</Link>
+          <Link to="/actividades" onClick={handleLinkClick}>Curso</Link>
         </li>
         <li>
           <Link to="/contacto" onClick={handleLinkClick}>Contacto</Link>
@@ -133,5 +133,6 @@ export default function Navbar() {
     </nav>
   );
 }
+
 
 
