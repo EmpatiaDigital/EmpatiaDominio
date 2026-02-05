3372bdf7c6585e249b8e4b6f4d668b5af5fc6385
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-04 23:40:43 -0300

    Update SuperAdminCourses.js

--- src/components/SuperAdminCourses.js
+++ src/components/SuperAdminCourses.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import '../style/SuperAdminCourses.css';
+import '../style/Superadmincourses.css';
 
 const SuperAdminCourses = () => {
   const { user } = useAuth();
