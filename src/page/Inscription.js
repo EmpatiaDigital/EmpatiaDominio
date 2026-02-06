24431dd6ba632989766ea1d0addbe381bfc73f3a
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-06 12:11:29 -0300

    Update Inscription.js

--- src/page/Inscription.js
+++ src/page/Inscription.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../style/Inscription.css';
 
+
 const Inscription = () => {
   const navigate = useNavigate();
   const [course, setCourse] = useState(null);
