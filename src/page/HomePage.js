34ad21db9df9b7163a9cd4aab18f24362abd3808
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-06-05 18:08:52 -0300

    Update HomePage.js

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../style/HomePage.css";
 import fondo1 from "../assets/Portada1.webp";
-import fondo2 from "../assets/Portada2.webp";
+import fondo2 from "../assets/Portada2.jpg";
 import fondo3 from "../assets/conexion.webp";
 import ModalActividades from "../components/ModalActividades";
 import PostStatsMini from "../components/PostStatsMini";
