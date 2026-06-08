2d5b5e16d1b127ce8802125648efe475b79905e5
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-06-08 14:59:06 -0300

    Update TestJuego.js

--- src/components/TestJuego.js
+++ src/components/TestJuego.js
@@ -279,11 +279,11 @@ const TestJuego = () => {
 
           <div className="tj-res-body">
             
-            {/* INSIGNIA PROFESIONAL EXCLUSIVA */}
+                  
+            {/* INSIGNIA PROFESIONAL EXCLUSIVA <img src="../assets/trivia.webp" alt="Insignia Background" className="tj-insignia-img" /> */}
             <div className={`tj-insignia-card ${configRango.clase}`}>
               <div className="tj-insignia-layout">
                 <div className="tj-insignia-asset-container">
-                  <img src="assets/trivia.webp" alt="Insignia Background" className="tj-insignia-img" />
                   <div className="tj-insignia-vector-overlay">
                     <svg viewBox="0 0 24 24" fill="none" stroke={configRango.color} strokeWidth={2}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
